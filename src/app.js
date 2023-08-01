dc26e8159a85a26cef2cc719a581cc062be534fa
gonchip <136641475+gonchip@users.noreply.github.com>
2023-08-01 22:51:34 +0000

    Tarea terminada

--- src/app.js
+++ src/app.js
@@ -1,11 +1,48 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["A ", "The "];
+  let subject = [
+    "jogger ",
+    "racoon ",
+    "dog ",
+    "driver ",
+    "comedian ",
+    "pincone "
+  ];
+  let action = [
+    "took my ",
+    "threw my ",
+    "yelled at my ",
+    "stole my ",
+    "bit my "
+  ];
+  let possetion = ["homework ", "toe ", "car ", "shoe "];
+  let where = ["on the street ", "in my house ", "in my driveway "];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subIndx = Math.floor(Math.random() * subject.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let possetionIndx = Math.floor(Math.random() * possetion.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    "" +
+    subject[subIndx] +
+    "" +
+    action[actionIndx] +
+    "" +
+    possetion[possetionIndx] +
+    "" +
+    where[whereIndx]
+  );
 };
